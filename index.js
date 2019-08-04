2d1c8a9e1463fc546dc0be447b526282f299b7f2
aniksa <aniksa@gmail.com>
2019-08-04 21:39:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -90,22 +90,3 @@ ctx.reply(' '+solveSap3(Number(s[1]),Number(s[2]),Number(s[3])));
 bot.launch();
 
 
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-
-
-
-
